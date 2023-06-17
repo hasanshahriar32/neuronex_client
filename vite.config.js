240ffa911198f3d1848baabcf7594b401cacf46d
Mr. Hasan <hasanshahriar32@gmail.com>
2023-06-17 12:22:46 +0600

    updated config and still debugging

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,21 @@ export default defineConfig({
   plugins: [react()],
   base: "/", // for github pages
   build: {
-    outDir: "dist", // for github pages
+    rollupOptions: {
+      output: {
+        assetFileNames: "_assets/[name].[ext]", // Optional: Organize assets in a separate directory
+      },
+    },
+    outDir: "dist",
+    assets: {
+      // Copy the _redirects file to the dist directory
+      // during the build process
+      copy: [
+        {
+          from: "_redirects",
+          to: "./",
+        },
+      ],
+    },
   },
 });
