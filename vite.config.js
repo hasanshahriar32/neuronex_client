b271830f27326d92878a1c2ec1db71e20d30f609
Mr. Hasan <hasanshahriar32@gmail.com>
2023-06-17 12:39:23 +0600

    code updated for deployment comfig

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // for github pages
+  base: "/",
   build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: "_assets/[name].[ext]", // Optional: Organize assets in a separate directory
-      },
-    },
     outDir: "dist",
-    assets: {
-      // Copy the _redirects file to the dist directory
-      // during the build process
-      copy: [
-        {
-          from: "_redirects",
-          to: "./",
-        },
-      ],
-    },
+  },
+  // Add the "postbuild" script to copy _redirects after the build
+  // is completed
+  scripts: {
+    postbuild: "cpy './_redirects' './dist'",
   },
 });
