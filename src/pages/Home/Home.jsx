8d6b8818626fae94925cc9133fe68492d2a03dbb
itzshakeeb <itzshakeeb@gmail.com>
2023-06-20 23:19:04 +0600

    added tailwind css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import OneTap from "../../components/Authentication/OneTap/OneTap";
 
 const Home = () => {
-  return (
-    <div>
-      <OneTap />
-      <p>this is home</p>
-    </div>
-  );
+    return (
+        <div>
+            <OneTap />
+            <h3 className="text-3xl font-bold underline">
+                Hello world!
+            </h3>
+        </div>
+    );
 };
 
 export default Home;
