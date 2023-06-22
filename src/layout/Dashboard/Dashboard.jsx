4483b0a63bd9f86cb7033c964699a979165d66f3
Mr. Hasan <hasanshahriar32@gmail.com>
2023-06-22 23:44:33 +0600

    dashboar

--- src/layout/Dashboard/Dashboard.jsx
+++ src/layout/Dashboard/Dashboard.jsx
@@ -19,7 +19,7 @@ const Dashboard = () => {
       </div>
       <div className="drawer-side z-40">
         <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-        <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
+        <ul className="menu p-4 w-[500px] h-full bg-base-200 text-base-content">
           {/* Sidebar content here */}
           <li>
             <a>Sidebar Item 1</a>
