13b6c49c851f190df9360d5c022ec422c5fbe1fc
Mr. Hasan <hasanshahriar32@gmail.com>
2023-06-23 12:43:50 +0600

    added chat form

--- src/pages/Compose/Compose.jsx
+++ src/pages/Compose/Compose.jsx
@@ -1,14 +1,15 @@
 import { useContext } from "react";
-import AiQuery from "../../components/FormComposed/AiQuery/AiQuery";
+// import AiQuery from "../../components/FormComposed/AiQuery/AiQuery";
 import AiSetting from "../../components/FormComposed/AiSetting/AiSetting";
 import { AiContext } from "../../components/FormComposed/FormContext/FormContext";
+import AiQuery2 from "../../components/FormComposed/AiQuery/AiQuery2";
 
 const Compose = () => {
   const { aiConfig } = useContext(AiContext);
   return (
     <div className="flex justify-center items-center min-h-screen min-w-full">
-      {aiConfig?.length === 0 ? <AiSetting /> : <AiQuery />}
-      {/* <AiQuery /> */}
+      {aiConfig?.length === 0 ? <AiSetting /> : <AiQuery2 />}
+      {/* <AiQuery2 /> */}
       {/* <iframe
         width="100%"
         height="800"
