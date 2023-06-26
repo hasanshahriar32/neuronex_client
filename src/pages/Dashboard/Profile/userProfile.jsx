70977c67f9d10c4dc0c4f832e8a56968ba8e6408
itzshakeeb <itzshakeeb@gmail.com>
2023-06-26 19:51:18 +0600

    added profile

---
+++ src/pages/Dashboard/Profile/userProfile.jsx
@@ -0,0 +1,12 @@
+import Profile from "../../../components/profile/Profile";
+
+
+const UserProfile = () => {
+    return (
+        <div>
+            <Profile />
+        </div>
+    );
+};
+
+export default UserProfile;
