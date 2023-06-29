2d1112c80aa803c512fd058e9b89311e03cf48f4
Mr. Hasan <hasanshahriar32@gmail.com>
2023-06-30 00:04:20 +0600

    updated sideCard ui

--- src/layout/Dashboard/Components/SideCard.jsx
+++ src/layout/Dashboard/Components/SideCard.jsx
@@ -38,21 +38,6 @@ const SideCard = ({ sesstionData }) => {
                       </p>
                     </div>
                     <div className="flex items-center">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-6 w-6 mr-2"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-                        />
-                      </svg>
-
                       <p className="font-medium">
                         {
                           new Date(session?.updatedAt)
