575a5040f205a5dda21ca012c5739973854abcca
itzshakeeb <itzshakeeb@gmail.com>
2023-07-03 00:25:09 +0600

    redesign login form

--- src/components/Authentication/AuthProvider/AuthProvider.jsx
+++ src/components/Authentication/AuthProvider/AuthProvider.jsx
@@ -4,91 +4,91 @@ import "react-toastify/dist/ReactToastify.css";
 import { AuthContext } from "../../../Contexts/UserContext/UserContext";
 
 const AuthProvider = () => {
-  const { handleGoogleSignIn, handleGithubSignIn } = useContext(AuthContext);
-  const handlegooglelogin = () => {
-    handleGoogleSignIn()
-      .then((res) => {
-        // console.log(res.user);
-        const name = res.user?.displayName;
-        const email = res.user?.email;
-        const uid = res.user?.uid;
-        const pic = res.user?.photoURL;
-        const verified = res.user?.emailVerified;
-        const userAbout = "member";
-        saveUserToDb(name, pic, email, uid, userAbout, verified);
-        const notify = () =>
-          toast.success("Login Successful", {
-            position: "bottom-center",
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "dark",
-          });
-        notify();
-      })
-      .catch((err) => {
-        console.log(err);
-        const notify = () => toast.error(err.message);
-        notify();
-      });
-  };
-  const handlegithublogin = () => {
-    handleGithubSignIn()
-      .then((res) => {
-        // console.log(res.user);
-        const name = res.user?.displayName;
-        const email = res.user?.email;
-        const pic = res.user?.photoURL;
-        const verified = res.user?.emailVerified;
-        const uid = res.user?.uid;
-        const userAbout = "member";
-        saveUserToDb(name, pic, email, uid, userAbout, verified);
-        const notify = () => toast.success("Login Successful");
-        notify();
-      })
-      .catch((err) => {
-        console.log(err);
-        const notify = () => toast.error(err.message);
-        notify();
-      });
-  };
-  const saveUserToDb = (name, pic, email, uid, userAbout, verified) => {
-    fetch("https://neuronex-server-test.vercel.app/user", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name, pic, email, uid, userAbout, verified }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log("save user", data);
-        // localStorage.setItem("user", JSON.stringify(data));
-        localStorage.setItem("token", JSON.stringify(data?.token));
-      });
-  };
-  return (
-    <div>
-      <ToastContainer />
-      <div className="">
-        <button
-          onClick={handlegooglelogin}
-          className="w-full tracking-wide btn-neutral text-xl btn h-10 btn-lg my-2"
-        >
-          login with google
-        </button>
-        <button
-          onClick={handlegithublogin}
-          className="w-full tracking-wide btn-neutral text-xl btn h-10 btn-lg my-2"
-        >
-          login with github
-        </button>
-      </div>
-    </div>
-  );
+    const { handleGoogleSignIn, handleGithubSignIn } = useContext(AuthContext);
+    const handlegooglelogin = () => {
+        handleGoogleSignIn()
+            .then((res) => {
+                // console.log(res.user);
+                const name = res.user?.displayName;
+                const email = res.user?.email;
+                const uid = res.user?.uid;
+                const pic = res.user?.photoURL;
+                const verified = res.user?.emailVerified;
+                const userAbout = "member";
+                saveUserToDb(name, pic, email, uid, userAbout, verified);
+                const notify = () =>
+                    toast.success("Login Successful", {
+                        position: "bottom-center",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "dark",
+                    });
+                notify();
+            })
+            .catch((err) => {
+                console.log(err);
+                const notify = () => toast.error(err.message);
+                notify();
+            });
+    };
+    const handlegithublogin = () => {
+        handleGithubSignIn()
+            .then((res) => {
+                // console.log(res.user);
+                const name = res.user?.displayName;
+                const email = res.user?.email;
+                const pic = res.user?.photoURL;
+                const verified = res.user?.emailVerified;
+                const uid = res.user?.uid;
+                const userAbout = "member";
+                saveUserToDb(name, pic, email, uid, userAbout, verified);
+                const notify = () => toast.success("Login Successful");
+                notify();
+            })
+            .catch((err) => {
+                console.log(err);
+                const notify = () => toast.error(err.message);
+                notify();
+            });
+    };
+    const saveUserToDb = (name, pic, email, uid, userAbout, verified) => {
+        fetch("https://neuronex-server-test.vercel.app/user", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ name, pic, email, uid, userAbout, verified }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log("save user", data);
+                // localStorage.setItem("user", JSON.stringify(data));
+                localStorage.setItem("token", JSON.stringify(data?.token));
+            });
+    };
+    return (
+        <div>
+            <ToastContainer />
+            <div className="">
+                <button
+                    onClick={handlegooglelogin}
+                    className="w-full tracking-wide btn-neutral  btn h-10 btn-lg my-1"
+                >
+                    login with google
+                </button>
+                <button
+                    onClick={handlegithublogin}
+                    className="w-full tracking-wide btn-neutral  btn h-10 btn-lg my-1"
+                >
+                    login with github
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default AuthProvider;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,66 +1,65 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import PrivateRoute from "../components/Authentication/PrivateRoute/PrivateRoute";
+import Profile from "../components/profile/Profile";
 import Dashboard from "../layout/Dashboard/Dashboard";
 import Main from "../layout/Main/Main";
+import ProfileLayout from "../layout/Profile/Profile";
+import ProfileEdit from "../layout/Profile/ProfileEdit";
 import Login from "../pages/Authentication/Login/Login";
 import Register from "../pages/Authentication/Register/Register";
 import Compose from "../pages/Dashboard/Compose/Compose";
 import Home from "../pages/Home/Home";
-import ProfileLayout from "../layout/Profile/Profile";
-import Profile from "../components/profile/Profile";
-import ProfileEdit from "../layout/Profile/ProfileEdit";
 
 const Routes = () => {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      children: [
-        { path: "/", element: <Home></Home> },
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <Main></Main>,
+            children: [
+                { path: "/", element: <Home></Home> },
+            ],
+        },
+        {
+            path: "/profile",
+            element: (
+                <PrivateRoute>
+                    {" "}
+                    <ProfileLayout></ProfileLayout>
+                </PrivateRoute>
+            ),
+            children: [
+                { path: "/profile/", element: <Profile></Profile> },
+                { path: "/profile/edit", element: <ProfileEdit></ProfileEdit> },
+            ],
+        },
+        {
+            path: "/ai",
+            element: (
+                <PrivateRoute>
+                    <Dashboard />
+                </PrivateRoute>
+            ),
+            children: [
+                {
+                    path: "/ai/",
+                    element: <Compose></Compose>,
+                },
 
+                {
+                    path: "/ai/compose",
+                    element: <Compose></Compose>,
+                },
+            ],
+        },
         { path: "/login", element: <Login></Login> },
         { path: "/register", element: <Register></Register> },
-      ],
-    },
-    {
-      path: "/profile",
-      element: (
-        <PrivateRoute>
-          {" "}
-          <ProfileLayout></ProfileLayout>
-        </PrivateRoute>
-      ),
-      children: [
-        { path: "/profile/", element: <Profile></Profile> },
-        { path: "/profile/edit", element: <ProfileEdit></ProfileEdit> },
-      ],
-    },
-    {
-      path: "/ai",
-      element: (
-        <PrivateRoute>
-          <Dashboard />
-        </PrivateRoute>
-      ),
-      children: [
-        {
-          path: "/ai/",
-          element: <Compose></Compose>,
-        },
-
-        {
-          path: "/ai/compose",
-          element: <Compose></Compose>,
-        },
-      ],
-    },
-  ]);
-  return (
-    <div className="bg-page-gradient pt-navigation-height">
-      {" "}
-      <RouterProvider router={router}></RouterProvider>
-    </div>
-  );
+    ]);
+    return (
+        <div className="bg-page-gradient pt-navigation-height">
+            {" "}
+            <RouterProvider router={router}></RouterProvider>
+        </div>
+    );
 };
 
 export default Routes;
