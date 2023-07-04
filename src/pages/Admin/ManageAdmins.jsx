dfb595b73c32c8d1867e4d23348ac8a2d7864216
itzshakeeb <itzshakeeb@gmail.com>
2023-07-05 00:23:59 +0600

    redesign manage admin profile

--- src/pages/Admin/ManageAdmins.jsx
+++ src/pages/Admin/ManageAdmins.jsx
@@ -334,7 +334,7 @@ const ManageAdmins = () => {
                                         <button
                                             id="actionsDropdownButton"
                                             data-dropdown-toggle="actionsDropdown"
-                                            className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200"
+                                            className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-background focus:z-10 focus:ring-4 focus:ring-gray-200 lg:hidden cursor-pointer"
                                             type="button"
                                         >
                                             <svg
@@ -381,7 +381,7 @@ const ManageAdmins = () => {
                                         <button
                                             id="filterDropdownButton"
                                             data-dropdown-toggle="filterDropdown"
-                                            className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                                            className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-background focus:z-10 focus:ring-4 focus:ring-gray-200 lg:hidden cursor-pointer"
                                             type="button"
                                         >
                                             <svg
