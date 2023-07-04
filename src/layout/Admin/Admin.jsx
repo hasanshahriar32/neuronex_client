f9fec9c5d8ae9ebfd592be1328c9c6a3805b7307
itzshakeeb <itzshakeeb@gmail.com>
2023-07-04 23:34:56 +0600

    redesign manage admin page

--- src/layout/Admin/Admin.jsx
+++ src/layout/Admin/Admin.jsx
@@ -2,26 +2,21 @@ import { Outlet } from "react-router";
 import AdminNavigation from "../../pages/Admin/AdminNavigation/AdminNavigation";
 
 const AdminLayout = () => {
-  return (
-    <div className="drawer lg:drawer-open">
-      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col items-center justify-center">
-        <label
-          htmlFor="my-drawer-2"
-          className="btn btn-primary drawer-button lg:hidden"
-        >
-          Open drawer
-        </label>
-        <Outlet></Outlet>
-      </div>
-      <div className="drawer-side">
-        <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-        <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
-          <AdminNavigation />
-        </ul>
-      </div>
-    </div>
-  );
+    return (
+        <div className="drawer lg:drawer-open absolute top-0">
+            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content flex flex-col items-center justify-center">
+
+                <Outlet></Outlet>
+            </div>
+            <div className="drawer-side">
+                <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+                <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
+                    <AdminNavigation />
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default AdminLayout;
