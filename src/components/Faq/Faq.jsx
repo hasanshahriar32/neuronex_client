cff83504d9c5533682c559e2ae8c899746016381
itzshakeeb <itzshakeeb@gmail.com>
2023-07-06 01:43:06 +0600

    added design faq

--- src/components/Faq/Faq.jsx
+++ src/components/Faq/Faq.jsx
@@ -1,79 +1,54 @@
-
+import img from "./images/illustration-woman-online-mobile.svg";
 const FAQ = () => {
     return (
-        <div className="mt-[100px]">
-            <section className="text-gray-700 text-md">
-                <div className="container px-5 py-24 mx-auto">
-                    <div className="text-center mb-20">
-                        <h1 className="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">
-                            Frequently Asked Questions
-                        </h1>
-                        <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
-                            The most common questions about how our business works and what can do for you.
-                        </p>
-                    </div>
-                    <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
-                        <div className="w-full lg:w-1/2 px-4 py-2">
-                            <details className="mb-4">
-                                <summary className="font-semibold bg-gray-200 rounded-md py-2 px-4">
-                                    How Long is this site live?
-                                </summary>
-                                <span>
-                                    Laboris qui labore cillum culpa in sunt quis sint veniam. Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                                    minim velit nostrud pariatur culpa magna in aute.
-                                </span>
+        <div className="my-[100px] ">
+
+            <div className="md:flex justify-around gap-4">
+                <div className="md:w-1/2 flex items-center justify-items-center">
+                    <img src={img} alt="illustration-woman-online" className="h-[380px] -ml-8 md:h-[300px] lg:h-[500px]" />
+                </div>
+
+                <div className="md:w-1/2 p-4 md:p-0 text-md">
+                    <div className="transform -translate-y-26 md:translate-y-0 mt-15 md:mb-20 flex flex-col justify-center ">
+                        <h1 className="text-5xl font-bold text-center md:text-left mb-10">FAQ</h1>
+                        <div className="mt-9.5 space-y-4">
+
+                            <details className="pb-5 border-b md:w-80 hover:cursor-pointer">
+                                <summary className="flex justify-between items-center text-summary md:text-sm font-normal text-very-dark-grayish-blue hover:text-soft-red">How many team members can I invite?</summary>
+                                <p className="text-dark-grayish-blue text-xs font-normal">You can invite up to 2 additional users on the Free plan. There is no limit on
+                                    team members for the Premium plan.</p>
                             </details>
-                            <details className="mb-4">
-                                <summary className="font-semibold bg-gray-200 rounded-md py-2 px-4">
-                                    Can I install/upload anything I want on there?
-                                </summary>
-                                <span>
-                                    Laboris qui labore cillum culpa in sunt quis sint veniam. Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                                    minim velit nostrud pariatur culpa magna in aute.
-                                </span>
+
+                            <details className="pb-5 border-b md:w-80 hover:cursor-pointer">
+                                <summary className="flex justify-between items-center text-summary md:text-sm font-normal text-very-dark-grayish-blue hover:text-soft-red">What is the maximum file upload size?</summary>
+                                <p className="text-dark-grayish-blue text-xs font-normal">No more than 2GB. All files in your account must fit your allotted storage space.</p>
                             </details>
-                            <details className="mb-4">
-                                <summary className="font-semibold bg-gray-200 rounded-md py-2 px-4">
-                                    How can I migrate to another site?
-                                </summary>
-                                <span>
-                                    Laboris qui labore cillum culpa in sunt quis sint veniam. Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                                    minim velit nostrud pariatur culpa magna in aute.
-                                </span>
+
+                            <details className="pb-5 border-b md:w-80 hover:cursor-pointer">
+                                <summary className="flex justify-between items-center text-summary md:text-sm font-normal text-very-dark-grayish-blue hover:text-soft-red">How do I reset my password?</summary>
+                                <p className="text-dark-grayish-blue text-xs font-normal">Click “Forgot password” from the login page or “Change password” from your profile page.
+                                    A reset link will be emailed to you.</p>
                             </details>
+
+                            <details className="pb-5 border-b md:w-80 hover:cursor-pointer">
+                                <summary className="flex justify-between items-center text-summary md:text-sm font-normal text-very-dark-grayish-blue hover:text-soft-red">Can I cancel my subscription?</summary>
+                                <p className="text-dark-grayish-blue text-xs font-normal">Yes! Send us a message and we’ll process your request no questions asked.</p>
+                            </details>
+
+                            <details className="pb-5 border-b md:w-80 hover:cursor-pointer">
+                                <summary className="flex justify-between items-center text-summary md:text-sm font-normal text-very-dark-grayish-blue hover:text-soft-red">Do you provide additional support?</summary>
+                                <p className="text-dark-grayish-blue text-xs font-normal">Chat and email support is available 24/7. Phone lines are open during normal business hours.</p>
+                            </details>
+
                         </div>
-                        <div className="w-full lg:w-1/2 px-4 py-2">
-                            <details className="mb-4">
-                                <summary className="font-semibold bg-gray-200 rounded-md py-2 px-4">
-                                    Can I change the domain you give me?
-                                </summary>
-                                <span className="px-4 py-2">
-                                    Laboris qui labore cillum culpa in sunt quis sint veniam. Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                                    minim velit nostrud pariatur culpa magna in aute.
-                                </span>
-                            </details>
-                            <details className="mb-4">
-                                <summary className="font-semibold bg-gray-200 rounded-md py-2 px-4">
-                                    How many sites I can create at once?
-                                </summary>
-                                <span className="px-4 py-2">
-                                    Laboris qui labore cillum culpa in sunt quis sint veniam. Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                                    minim velit nostrud pariatur culpa magna in aute.
-                                </span>
-                            </details>
-                            <details className="mb-4">
-                                <summary className="font-semibold bg-gray-200 rounded-md py-2 px-4">
-                                    How can I communicate with you?
-                                </summary>
-                                <span className="px-4 py-2">
-                                    Laboris qui labore cillum culpa in sunt quis sint veniam. Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                                    minim velit nostrud pariatur culpa magna in aute.
-                                </span>
-                            </details>
-                        </div>
+
                     </div>
                 </div>
-            </section>
+            </div>
+
+
+
+
         </div>
     );
 };
