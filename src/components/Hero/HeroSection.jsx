067a7ace00975ae59a4b4ad6a033efbd60a3e475
itzshakeeb <itzshakeeb@gmail.com>
2023-07-07 18:37:54 +0600

    hero section while in view problem solved

--- src/components/Hero/HeroSection.jsx
+++ src/components/Hero/HeroSection.jsx
@@ -1,41 +1,43 @@
+import { Link } from "react-router-dom";
 import { Button, Highlight } from "../features/Button";
 import HeroBtn from "./HeroBtn/HeroBtn";
 import { Hero, HeroSubtitle, HeroTitle } from "./hero";
 import { HeroImage } from "./hero_img";
 
 const HeroSection = () => {
-  return (
-    <div>
-      <Hero>
-        <Button
-          className="translate-y-[-1rem] animate-fade-in opacity-0"
-          href="/"
-          variant="secondary"
-          size="small"
-        >
-          <span className="pr-2 flex flex-row gap-1">
-            {" "}
-            <span>NeuroNex 2023 Release </span> <span className="hidden md:block">– Built for Education</span>
-          </span>
-          <Highlight>→</Highlight>
-        </Button>
-        <HeroTitle className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:200ms]">
-          NeuroNex: the better
-          <br className="hidden md:block" /> way to Learn
-        </HeroTitle>
-        <HeroSubtitle className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms]">
-          Learning Journeys, Revolutionizing Education with Transformative
-          <br className="hidden md:block" />
-          Solutions for Personal Growth and Empowered Futures
-        </HeroSubtitle>
-        <div className="flex justify-center w-full">
-          <HeroBtn />
+    return (
+        <div className="">
+            <Hero>
+                <Button
+                    className="translate-y-[-1rem] animate-fade-in opacity-0"
+                    href="/"
+                    variant="secondary"
+                    size="small"
+                >
+                    <Link to="/team" className="pr-2 flex flex-row gap-1">
+                        {" "}
+                        <span>NeuroNex 2023 Release </span> <span className="hidden md:block">– Built for Education</span>
+                    </Link>
+                    <Highlight>→</Highlight>
+                </Button>
+                <HeroTitle className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:200ms]">
+                    NeuroNex: the better
+                    <br className="hidden md:block" /> way to Learn
+                </HeroTitle>
+                <HeroSubtitle className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms]">
+                    Learning Journeys, Revolutionizing Education with Transformative
+                    <br className="hidden md:block" />
+                    Solutions for Personal Growth and Empowered Futures
+                </HeroSubtitle>
+                <div className="flex justify-center w-full">
+                    <HeroBtn />
+                </div>
+                <div className="">
+                    <HeroImage />
+                </div>
+            </Hero>
         </div>
-
-        <HeroImage />
-      </Hero>
-    </div>
-  );
+    );
 };
 
 export default HeroSection;
