2dca853916a83e15457fd8872abfa25185bfb69f
Mr. Hasan <hasanshahriar32@gmail.com>
2023-07-07 12:14:06 +0600

    updated

--- src/components/Payment/ CheckoutForm.jsx
+++ src/components/Payment/ CheckoutForm.jsx
@@ -35,7 +35,7 @@ const CheckoutForm = ({ packagE, setPackage, agreeTerms, setModalOpen }) => {
   const addToDb = (product) => {
     setModalOpen(true);
     console.log(product);
-    fetch("http://localhost:5000/payment/resolve-intent", {
+    fetch("https://neuronex-server-test.vercel.app/payment/resolve-intent", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
