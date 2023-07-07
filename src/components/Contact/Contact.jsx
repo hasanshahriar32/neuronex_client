a241b42064832abc14e1173ce94223f37aa493db
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2023-07-07 11:11:45 +0600

    commit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -70,8 +70,7 @@ const Contact = () => {
                             type="email"
                             className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0 border-b border-neutral-500"
                             id="exampleInput91"
-                            placeholder="Email
-                                                 address"
+                            placeholder="Email  address"
                             required
                             name="user_email"
                           />
