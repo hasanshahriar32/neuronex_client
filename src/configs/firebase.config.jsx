2ee3d3ac906c711d02fe3d511c90e471674a80af
itzshakeeb <itzshakeeb@gmail.com>
2023-07-12 15:53:30 +0600

    added firebase config in .env

--- src/configs/firebase.config.jsx
+++ src/configs/firebase.config.jsx
@@ -7,13 +7,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyATDkaNewClQmzsFS3MpowUhv95wGhKDmA",
-  authDomain: "neuronex-ai.firebaseapp.com",
-  projectId: "neuronex-ai",
-  storageBucket: "neuronex-ai.appspot.com",
-  messagingSenderId: "179292359244",
-  appId: "1:179292359244:web:cb19471c43826421aac2e9",
-  measurementId: "G-6FVD96DP1T",
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId,
+    measurementId: import.meta.env.VITE_measurementId,
 };
 
 // Initialize Firebase
