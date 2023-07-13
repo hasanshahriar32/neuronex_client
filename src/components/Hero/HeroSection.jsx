84df7694dab4fc9c7525b2ac59cd92259804311b
Mr. Hasan <hasanshahriar32@gmail.com>
2023-07-13 23:58:22 +0600

    video link added

--- src/components/Hero/HeroSection.jsx
+++ src/components/Hero/HeroSection.jsx
@@ -5,39 +5,40 @@ import { Hero, HeroSubtitle, HeroTitle } from "./hero";
 import { HeroImage } from "./hero_img";
 
 const HeroSection = () => {
-    return (
-        <div className="">
-            <Hero>
-                <Button
-                    className="translate-y-[-1rem] animate-fade-in opacity-0"
-                    href="/"
-                    variant="secondary"
-                    size="small"
-                >
-                    <Link to="/team" className="pr-2 flex flex-row gap-1">
-                        {" "}
-                        <span>NeuroNex 2023 Release </span> <span className="hidden md:block">– Built for Education</span>
-                    </Link>
-                    <Highlight>→</Highlight>
-                </Button>
-                <HeroTitle className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:200ms]">
-                    NeuroNex: the better
-                    <br className="hidden md:block" /> way to Learn
-                </HeroTitle>
-                <HeroSubtitle className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms]">
-                    Learning Journeys, Revolutionizing Education with Transformative
-                    <br className="hidden md:block" />
-                    Solutions for Personal Growth and Empowered Futures
-                </HeroSubtitle>
-                <div className="flex justify-center w-full">
-                    <HeroBtn />
-                </div>
-                <div className="">
-                    <HeroImage />
-                </div>
-            </Hero>
+  return (
+    <div className="">
+      <Hero>
+        <Button
+          className="translate-y-[-1rem] animate-fade-in opacity-0"
+          href="/"
+          variant="secondary"
+          size="small"
+        >
+          <Link to="/docs" className="pr-2 flex flex-row gap-1">
+            {" "}
+            <span>NeuroNex 2023 Release </span>{" "}
+            <span className="hidden md:block">– Built for Education</span>
+          </Link>
+          <Highlight>→</Highlight>
+        </Button>
+        <HeroTitle className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:200ms]">
+          NeuroNex: the better
+          <br className="hidden md:block" /> way to Learn
+        </HeroTitle>
+        <HeroSubtitle className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms]">
+          Learning Journeys, Revolutionizing Education with Transformative
+          <br className="hidden md:block" />
+          Solutions for Personal Growth and Empowered Futures
+        </HeroSubtitle>
+        <div className="flex justify-center w-full">
+          <HeroBtn />
         </div>
-    );
+        <div className="">
+          <HeroImage />
+        </div>
+      </Hero>
+    </div>
+  );
 };
 
 export default HeroSection;
